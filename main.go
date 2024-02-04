773bc6430f85d764a436f6143dfd46af25fc757b
SalgotraRishav <salgotra_rishav@yahoo.in>
2024-02-04 01:01:23 -0500

    Fixed Hammad's code and added the changes of Rishav in main.go by creating a new branch - rishav

--- main.go
+++ main.go
@@ -10,18 +10,20 @@ func main() {
 	result := add(3, 7)
 	fmt.Println("Sum:", result)
 
-
-  // Start of Hammad Ul Hassan - 500230292 code
-  fmt.Printf("Hammad Ul Hassan - 500230292\n")
-  fmt.Printf("Enter a number to check if it's even or odd:\n")
+	// Start of Hammad Ul Hassan - 500230292 code
+	fmt.Printf("Hammad Ul Hassan - 500230292\n")
+	fmt.Printf("Enter a number to check if it's even or odd:\n")
 	var num int
 	fmt.Scanln(&num)
-  result := isEven(num)
-  fmt.Printf("%d is %s\n", num, result)
-  // End of Hammad Ul Hassan - 500230292 code
+	//  result := isEven(num)
+	fmt.Printf("%d is %s\n", num, isEven(num))
+	// End of Hammad Ul Hassan - 500230292 code
 
+	print("\n*** Rishav's code ***\n")
 
-
+	// start of Rishav's code - 500228178
+	greatestOfThree()
+	// end of Rishav's code - 500228178
 }
 func add(a, b int) int {
 	return a + b
@@ -36,19 +38,48 @@ func revString(str string) string {
 	return strOutput
 }
 
-
-
-
 // Created by Hammad Ul Hassan - 500230292
 // This program checks if a number is even or odd. If the number is even, it returns "Even", else it returns "Odd".
 // The logic is simple, if the number is divisible by 2, it is even, else it is odd which is verified by the if statement.
 // It asks the user to enter a number and then calls the isEven function to check if the number is even or odd.
 // The result is then printed on the screen.
 func isEven(number int) string {
-  if number%2 == 0 {
-      return "Even"
-  } else {
-      return "Odd"
-  }
+	if number%2 == 0 {
+		return "Even"
+	} else {
+		return "Odd"
+	}
 }
 
+func greatestOfThree() {
+	// Prompt user for input
+	fmt.Println("Enter three numbers:")
+
+	// Taking three variables of integer type
+	var num1, num2, num3 int
+
+	// Read inputs from user and save in num1, num2 and num3 variables
+	fmt.Print("Number 1: ")
+	fmt.Scanln(&num1)
+	fmt.Print("Number 2: ")
+	fmt.Scanln(&num2)
+	fmt.Print("Number 3: ")
+	fmt.Scanln(&num3)
+
+	// Calling the 'greatest' function and storing the return value in num variable
+	num := greatest(num1, num2, num3)
+
+	// Displaying the output on the screen
+	fmt.Println(num, "is the greatest\n")
+}
+
+// function to calculate the greatest out of three
+func greatest(a, b, c int) int {
+	if a >= b && a >= c {
+		return a
+	} else if b >= a && b >= c {
+		return b
+	} else {
+		return c
+	}
+}
